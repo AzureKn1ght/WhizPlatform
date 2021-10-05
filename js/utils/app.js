22b942ff36a8bd4bc603ad6bdad6d4f836da1893
Kenneth Cheong <clarinet.kenneth@gmail.com>
2021-10-05 15:43:03 +0800

    static page for gigs

--- js/utils/app.js
+++ js/utils/app.js
@@ -180,3 +180,137 @@ const app = {
     }
   }
 };
+
+
+
+
+const express = require('express');
+const bodyParser = require("body-parser");
+const path = require('path');
+const Joi = require('joi');
+
+const db = require("./db");
+const collection = "jobs";
+const app2 = express();
+
+// schema used for data validation for our todo document
+const schema = Joi.object().keys({
+  title:   Joi.string().required(),
+  budget: Joi.number().required(),
+  description : Joi.string().required(),
+  deadline: Joi.date().required(),
+  skills_required: Joi.array().required(),
+  required_location: Joi.string().required(),
+  job_hirer : Joi.string().required()
+
+});
+
+// parses json data sent to us by the user 
+// app.use(bodyParser.json());
+
+// serve static html file to user
+app2.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../../gigs.html'));
+});
+
+// read
+app2.get('/getTodos',(req,res)=>{
+    // get all Todo documents within our todo collection
+    // send back to user as json
+    db.getDB().collection(collection).find({}).toArray((err,documents)=>{
+        if(err)
+            console.log(err);
+        else{
+            res.json(documents);
+        }
+    });
+});
+
+// update
+app2.put('/:id',(req,res)=>{
+    // Primary Key of Todo Document we wish to update
+    const todoID = req.params.id;
+    // Document used to update
+    const userInput = req.body;
+    // Find Document By ID and Update
+    db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(todoID)},
+    {$set : {description : userInput.description}},{returnOriginal : false},(err,result)=>{
+        if(err)
+            console.log(err);
+        else{
+            res.json(result);
+        }      
+    });
+});
+
+
+//create
+app2.post('/',(req,res,next)=>{
+    // Document to be inserted
+    const userInput = req.body;
+
+    // Validate document
+    // If document is invalid pass to error middleware
+    // else insert document within todo collection
+    Joi.validate(userInput,schema,(err,result)=>{
+        if(err){
+            const error = new Error("Invalid Input");
+            error.status = 400;
+            next(error);
+        }
+        else{
+            db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+                if(err){
+                    const error = new Error("Failed to insert Todo Document");
+                    error.status = 400;
+                    next(error);
+                }
+                else
+                    res.json({result : result, document : result.ops[0],msg : "Successfully inserted Todo!!!",error : null});
+            });
+        }
+    })    
+});
+
+
+
+//delete
+app2.delete('/:id',(req,res)=>{
+    // Primary Key of Todo Document
+    const todoID = req.params.id;
+    // Find Document By ID and delete document from record
+    db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(todoID)},(err,result)=>{
+        if(err)
+            console.log(err);
+        else
+            res.json(result);
+    });
+});
+
+// Middleware for handling Error
+// Sends Error Response Back to User
+app2.use((err,req,res,next)=>{
+    res.status(err.status).json({
+        error : {
+            message : err.message
+        }
+    });
+})
+
+
+db.connect((err)=>{
+    // If err unable to connect to database
+    // End application
+    if(err){
+        console.log('unable to connect to database');
+        process.exit(1);
+    }
+    // Successfully connected to database
+    // Start up our Express Application
+    // And listen for Request
+    else{
+        app2.listen(3000,()=>{
+            console.log('connected to database, app listening on port 3000');
+        });
+    }
+});
