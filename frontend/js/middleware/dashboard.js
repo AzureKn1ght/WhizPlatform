364e490e14aebd61ae87da1d827c2b5c2c6fbf30
Anwar <anwartan8@gmail.com>
2021-10-21 22:11:35 +0800

    recommended gigs dynamic

--- frontend/js/middleware/dashboard.js
+++ frontend/js/middleware/dashboard.js
@@ -1,7 +1,36 @@
 const gigsInProgress = document.getElementById("gigs-in-progress");
 const gigsAppliedFor = document.getElementById("gigs-applied-for");
+const gigsRecommendedTo = document.getElementById("gigs-recommended")
 const gigsCompletedCard = document.getElementById("gigs-completed");
+const receivedReviews = document.getElementById("reviews-received");
 const user = window.sessionStorage.getItem("userId");
+const meta = window.sessionStorage.getItem("accountId");
+const web3 = new Web3(window.ethereum);
+const conAddress = "0xfb8362626ddE20BC9b8f4e323d49b52D89dD98c8";
+const contract = new web3.eth.Contract(abi, conAddress);
+var reviewsData = [
+  {
+    reviewID: 0,
+    reviewer: "0x4b7d0309042Be687F432B6fB73BCbcd405CD25e5",
+    reviewee: "0x4b7d0309042Be687F432B6fB73BCbcd405CD25e5",
+    ratings: {
+      overall: 4,
+      communication: 3,
+      quality: 2,
+      timeliness: 1,
+      grading: 2,
+    },
+    comments: "He was very professional!",
+    job: {
+      jobID: "1234",
+      budget: 2300,
+      title: "Code a html website",
+      description: "Design html code for frontend of my webshop.",
+      skills: ["html", "web design", "frontend"],
+      location: "Singapore",
+    },
+  },
+];
 
 function loadScript(url) {
   // Adding the script tag to the head as suggested before
@@ -50,7 +79,7 @@ const gigsProgress = async () => {
       console.log(file);
       console.log(typeof file);
       console.log(gigs._id);
-      console.log(typeof(gigs._id));
+      console.log(typeof gigs._id);
 
       //Add to element
       elm += `<!-- USER PREVIEW -->
@@ -157,7 +186,7 @@ const gigsProgress = async () => {
               <!-- USER STAT -->
               <div class="user-stat">
                 <!-- USER STAT TITLE -->
-                <p class="user-stat-title">USD${gigs.budget}</p>
+                <p class="user-stat-title">USD ${gigs.budget}</p>
                 <!-- /USER STAT TITLE -->
 
                 <!-- USER STAT TEXT -->
@@ -233,7 +262,7 @@ const updateStatus = async (e) => {
   window.location.href = "complete-gig-fl.html";
 };
 
-const gigsApplied =async () => {
+const gigsApplied = async () => {
   let url =
     "https://ap-southeast-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/whiz-ihwsd/service/jobs/incoming_webhook/appliedJobs";
   let elm = "";
@@ -253,7 +282,6 @@ const gigsApplied =async () => {
     const data = await res.json();
     console.log(data);
     data.gigs.forEach((gigs) => {
-    
       console.log(gigs);
       var image = Math.floor(Math.random() * 3) + 17;
       var profilePic = "img/marketplace/items/" + image + ".jpg";
@@ -261,8 +289,11 @@ const gigsApplied =async () => {
 
       var image2 = Math.floor(Math.random() * 9) + 1;
       console.log(image2);
-      console.log(typeof(image2));
-      var profilePic2 = (image2<10) ? "img/avatar/0" + image2 + ".jpg" : "img/avatar/" + image2 + ".jpg";
+      console.log(typeof image2);
+      var profilePic2 =
+        image2 < 10
+          ? "img/avatar/0" + image2 + ".jpg"
+          : "img/avatar/" + image2 + ".jpg";
       var file2 = profilePic2.toString();
 
       console.log(file);
@@ -330,48 +361,179 @@ const gigsApplied =async () => {
         </div>
         <!-- /PRODUCT PREVIEW META -->
       </div>
-      <!-- /PRODUCT PREVIEW -->` });
-      gigsAppliedFor.innerHTML = elm;
+      <!-- /PRODUCT PREVIEW -->`;
+    });
+    gigsAppliedFor.innerHTML = elm;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-    } catch(error){
-      console.log(error.message);
-    }
+
+
+const gigsRecommeded = async () => {
+  let urlFlSkills =
+    "https://ap-southeast-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/whiz-ihwsd/service/freelancers/incoming_webhook/viewFlSkills";
+  let elm = "";
+  const freelancer = {
+    _id: user,
   };
 
-
-  const gigsCompleted =async () => {
-    let url =
-      "https://ap-southeast-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/whiz-ihwsd/service/jobs/incoming_webhook/completedJobs";
-    let elm = "";
-    const freelancer = {
-      applicant: user,
+  try {
+    const res = await fetch(urlFlSkills, {
+      method: "POST",
+      body: JSON.stringify(freelancer),
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+    let skills = data.skills;
+    let skills_required = {
+      skills: skills,
     };
+    
+    console.log(skills);
+    console.log(skills_required);
+
+    
+
+  let urlRecommendedJobs =
+  "https://ap-southeast-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/whiz-ihwsd/service/jobs/incoming_webhook/recommendedJobs"
   
-    try {
-      const res = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(freelancer),
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await res.json();
-      console.log(data);
-      data.gigs.forEach((gigs) => {
-      
-        console.log(gigs);
-        var date = moment(gigs.deadline).format("DD MMM YYYY");
-        /* var image = Math.floor(Math.random() * 3) + 17;
+    const res2 = await fetch(urlRecommendedJobs, {
+      method: "POST",
+      body: JSON.stringify(skills_required),
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data2 = await res2.json();
+    console.log(data2);
+    data2.gigs.forEach((gigs) => {
+      console.log(gigs);
+      var image = Math.floor(Math.random() * 3) + 17;
+      var profilePic = "img/marketplace/items/" + image + ".jpg";
+      var file = profilePic.toString();
+
+      var image2 = Math.floor(Math.random() * 9) + 1;
+      console.log(image2);
+      console.log(typeof image2);
+      var profilePic2 =
+        image2 < 10
+          ? "img/avatar/0" + image2 + ".jpg"
+          : "img/avatar/" + image2 + ".jpg";
+      var file2 = profilePic2.toString();
+
+      console.log(file);
+      console.log(typeof file);
+      console.log(file2);
+      console.log(typeof file2);
+
+      //Add to element
+      elm += `<!-- PRODUCT PREVIEW -->
+      <div class="product-preview">
+        <!-- PRODUCT PREVIEW IMAGE -->
+        <a href="marketplace-product.html">
+          <figure class="product-preview-image liquid">
+            <img src="${file}" alt="item-01">
+          </figure>
+        </a>
+        <!-- /PRODUCT PREVIEW IMAGE -->
+
+        <!-- PRODUCT PREVIEW INFO -->
+        <div class="product-preview-info">
+          <!-- TEXT STICKER -->
+          <p class="text-sticker"><span class="highlighted">$</span> ${gigs.budget}</p>
+          <!-- /TEXT STICKER -->
+
+          <!-- PRODUCT PREVIEW TITLE -->
+          <p class="product-preview-title"><a href="marketplace-product.html">${gigs.title}</a></p>
+          <!-- /PRODUCT PREVIEW TITLE -->
+
+          <!-- PRODUCT PREVIEW CATEGORY -->
+          <p class="product-preview-category digital"><a href="marketplace-category.html">${gigs.skills_required}</a></p>
+          <!-- /PRODUCT PREVIEW CATEGORY -->
+
+          <!-- PRODUCT PREVIEW TEXT -->
+          <p class="product-preview-text">${gigs.description}</p>
+          <!-- /PRODUCT PREVIEW TEXT -->
+        </div>
+        <!-- /PRODUCT PREVIEW INFO -->
+
+        <!-- PRODUCT PREVIEW META -->
+        <div class="product-preview-meta">
+          <!-- PRODUCT PREVIEW AUTHOR -->
+          <div class="product-preview-author">
+            <!-- PRODUCT PREVIEW AUTHOR IMAGE -->
+            <a class="product-preview-author-image user-avatar micro no-border" href="profile-timeline.html">
+              <!-- USER AVATAR CONTENT -->
+              <div class="user-avatar-content">
+                <!-- HEXAGON -->
+                <div class="hexagon-image-18-20" data-src="${file2}"></div>
+                <!-- /HEXAGON -->
+              </div>
+              <!-- /USER AVATAR CONTENT -->
+            </a>
+            <!-- /PRODUCT PREVIEW AUTHOR IMAGE -->
+
+            <!-- PRODUCT PREVIEW AUTHOR TITLE -->
+            <p class="product-preview-author-title">Posted By</p>
+            <!-- /PRODUCT PREVIEW AUTHOR TITLE -->
+
+            <!-- PRODUCT PREVIEW AUTHOR TEXT -->
+            <p class="product-preview-author-text"><a href="profile-timeline.html">${gigs.job_hirer}</a></p>
+            <!-- /PRODUCT PREVIEW AUTHOR TEXT -->
+          </div>
+          <!-- /PRODUCT PREVIEW AUTHOR -->
+          <!-- /RATING LIST -->
+        </div>
+        <!-- /PRODUCT PREVIEW META -->
+      </div>
+      <!-- /PRODUCT PREVIEW -->`;
+    });
+    gigsRecommendedTo.innerHTML = elm;
+
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+
+
+
+
+const gigsCompleted = async () => {
+  let url =
+    "https://ap-southeast-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/whiz-ihwsd/service/jobs/incoming_webhook/completedJobs";
+  let elm = "";
+  const freelancer = {
+    applicant: user,
+  };
+
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(freelancer),
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+    data.gigs.forEach((gigs) => {
+      console.log(gigs);
+      var date = moment(gigs.deadline).format("DD MMM YYYY");
+      /* var image = Math.floor(Math.random() * 3) + 17;
         var profilePic = "img/marketplace/items/" + image + ".jpg";
         var file = profilePic.toString(); */
-  
-       
-  
-       
-  
-        //Add to element
-        elm += `<div class="user-preview landscape">
+
+      //Add to element
+      elm += `<div class="user-preview landscape">
         <!-- USER PREVIEW COVER -->
         <figure class="user-preview-cover liquid">
           <img src="img/cover/29.jpg" alt="cover-29">
@@ -456,16 +618,169 @@ const gigsApplied =async () => {
           <!-- /USER PREVIEW ACTIONS -->
         </div>
         <!-- /USER PREVIEW INFO -->
-      </div>` });
-        gigsCompletedCard.innerHTML = elm;
-  
-      } catch(error){
-        console.log(error.message);
-      }
-    };
+      </div>`;
+    });
+    gigsCompletedCard.innerHTML = elm;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
+const getReview = async () => {
+  console.log("Current account is:" + meta);
+  let elm = "";
+  
+  try {
+    const result = await contract.methods.getAllReceivedReviews(meta).call();
+    console.log(result);
+    const data = await result;
+    reviewsData = data;
+    console.log(reviewsData);
+    
+
+    reviewsData.forEach((reviews) => {
+      let jobName = reviews.job.title;
+      let grading = reviews.ratings.grading;
+      let text = "";
+      switch (parseInt(grading)) {
+        case 0:
+          text = "Nil";
+          console.log("switch worked case 0 activated");
+          break;
+        case 1:
+          text = "Exceeds Expectations";
+          console.log("switch worked case 0 activated");
+          break;
+        case 2:
+          text = "Meets Expectations";
+          console.log("switch worked case 0 activated");
+      };
+     /*  if(grading=0){
+        text = "Nil";
+      }else if(grading=1){
+        text = "Exceeds Expectations";
+      } else{
+        text = "Meets Expectations";
+      } */
+    
+      console.log("grading is:" + grading);
+      console.log("text is " + text)
+      let overall = reviews.ratings.overall;
+      let quality = reviews.ratings.quality;
+      let communication = reviews.ratings.communication;
+      let timeliness = reviews.ratings.timeliness;
+
+      elm += `
+      <div class="user-preview landscape">
+      <!-- USER PREVIEW COVER -->
+      <figure class="user-preview-cover liquid">
+        <img src="img/cover/29.jpg" alt="cover-29">
+      </figure>
+      <!-- /USER PREVIEW COVER -->
+
+      <!-- USER PREVIEW INFO -->
+      <div class="user-preview-info">
+        <!-- USER SHORT DESCRIPTION -->
+        <div class="user-short-description landscape tiny">
+          <!-- USER SHORT DESCRIPTION AVATAR -->
+          <a class="user-short-description-avatar user-avatar small no-stats" href="group-timeline.html">
+
+            <!-- /USER AVATAR CONTENT -->
+          </a>
+          <!-- /USER SHORT DESCRIPTION AVATAR -->
+
+          <!-- USER SHORT DESCRIPTION TITLE -->
+          <p class="user-short-description-title"><a href="group-timeline.html">
+            ${jobName}
+          </a></p>
+          <!-- /USER SHORT DESCRIPTION TITLE -->
+
+          <!-- USER SHORT DESCRIPTION TEXT -->
+          <p class="user-short-description-text">${text}</p>
+          <!-- /USER SHORT DESCRIPTION TEXT -->
+        </div>
+        <!-- /USER SHORT DESCRIPTION -->
+
+        <!-- USER STATS -->
+        <div class="user-stats">
+          <!-- USER STAT -->
+          <div class="user-stat">
+            <!-- USER STAT TITLE -->
+            <p class="user-stat-title">${overall}</p>
+            <!-- /USER STAT TITLE -->
+
+            <!-- USER STAT TEXT -->
+            <p class="user-stat-text">Overall</p>
+            <!-- /USER STAT TEXT -->
+          </div>
+          <!-- /USER STAT -->
+
+          <!-- USER STAT -->
+          <div class="user-stat">
+            <!-- USER STAT TITLE -->
+            <p class="user-stat-title">${quality}</p>
+            <!-- /USER STAT TITLE -->
+
+            <!-- USER STAT TEXT -->
+            <p class="user-stat-text">Quality</p>
+            <!-- /USER STAT TEXT -->
+          </div>
+          <!-- /USER STAT -->
+
+          <!-- USER STAT -->
+          <div class="user-stat">
+            <!-- USER STAT TITLE -->
+            <p class="user-stat-title">${communication}</p>
+            <!-- /USER STAT TITLE -->
+
+            <!-- USER STAT TEXT -->
+            <p class="user-stat-text">Communication</p>
+            <!-- /USER STAT TEXT -->
+          </div>
+          <!-- /USER STAT -->
+
+          <!-- USER STAT -->
+          <div class="user-stat">
+            <!-- USER STAT TITLE -->
+            <p class="user-stat-title">${timeliness}</p>
+            <!-- /USER STAT TITLE -->
+
+            <!-- USER STAT TEXT -->
+            <p class="user-stat-text">Timeliness</p>
+            <!-- /USER STAT TEXT -->
+          </div>
+          <!-- /USER STAT -->
+
+        </div>
+        <!-- /USER STATS -->
+
+        <!-- USER PREVIEW ACTIONS -->
+        <div class="user-preview-actions">
+          <!-- BUTTON -->
+          <p class="button secondary">
+            <!-- ICON INFO -->
+            <svg class="button-icon icon-info">
+              <use xlink:href="#svg-info"></use>
+            </svg>
+            <!-- /ICON INFO -->
+          </p>
+          <!-- /BUTTON -->
+        </div>
+        <!-- /USER PREVIEW ACTIONS -->
+      </div>
+      <!-- /USER PREVIEW INFO -->
+    </div>
+      `;
+    });
+
+    receivedReviews.innerHTML = elm;
+  } catch (error) {
+    console.log("No reviews found:" + error);
+  }
+};
 
 gigsProgress();
 gigsApplied();
+gigsRecommeded();
 gigsCompleted();
-
+getReview();
