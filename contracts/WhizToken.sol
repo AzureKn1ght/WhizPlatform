d67fa50bd04a9f63d622e40d83718127e6dd8c7d
AzureKn1ght <philsonnah@msn.com>
2021-10-22 17:07:11 +0800

    Updated Whiz Token

--- contracts/WhizToken.sol
+++ contracts/WhizToken.sol
@@ -1,11 +1,12 @@
-pragma solidity ^0.6.2;
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.1.0/contracts/token/ERC20/ERC20.sol"; 
+// SPDX-License-Identifier: MPL-2.0-no-copyleft-exception
+pragma solidity ^0.8.7;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol"; 
 
-contract PhilsonToken is ERC20 {
+contract WhizToken is ERC20 {
     
-    constructor () public ERC20("Whiz Token", "WHIZ") 
+    constructor () ERC20("Whiz Token", "WHIZ") 
     { 
-        _mint(msg.sender, 10000 * (10 ** uint256(decimals())));
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
     } 
         
 } 
