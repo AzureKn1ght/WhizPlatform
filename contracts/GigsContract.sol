e1febd41ce1b32c0cf488b27e33fdfac28b8feb0
AzureKn1ght <philsonnah@msn.com>
2021-10-22 18:26:40 +0800

    GigsContract WIP

--- contracts/GigsContract.sol
+++ contracts/GigsContract.sol
@@ -1,47 +1,56 @@
 // SPDX-License-Identifier: MPL-2.0-no-copyleft-exception
 pragma solidity ^0.8.7;
+import "./ReviewsContract.sol";
+import "./WhizToken.sol";
 
 contract GigsContract {
     
     //VARIABLES
     address admin;
+    uint public whizRewards;
     uint256 public totalValueLocked;
     enum Status { AWAITING_PAYMENT, AWAITING_DELIVERY, COMPLETED, RESOLVED }
 
-    //This is the USDC erc20 contract address
-    //address constant USDCaddress = 0;
+    //References to external contract addresses
+    address USDCaddress;
+    address WHIZaddress;
+    address REVIEWSaddress;
 
-    //Object for storing Gig details
+
+    //Create the object for storing all Gig details
     struct Gig {
         address payable hirer;
         address payable freelancer;
-        string jobDetails; //to update with Job Obj later
-        uint amount;
+        ReviewsContract.Job jobDetails; //Job Obj
+        uint amountDeposited;
         Status status;
     }
 
+
     //Every gig is mapped to a gigID string
     mapping (string => Gig) public gig;
+    mapping(address => mapping(string => Gig)) public myGigs; //ideal state: mapping of mappings
     
     //Events log
     event CreateGig(string gigID, Gig gig, string msg);
     event ResolveGig(string gigID, Gig gig, string msg);
     event CompleteGig(string gigID, Gig gig, string msg);
 
-
-    //Initialize contract as admin
+    //Initialize contract admin
     constructor() {
         admin = msg.sender;
     }
     
-    //MODIFIERS
+    //Modifier - Secure admin functions
     modifier onlyAdmin() {
-        require(msg.sender == admin, "Only admin can unlock escrow.");
+        require(msg.sender == admin, "Only the admin can access this feature.");
         _;
     }
     
     
-    //FUNCTIONS
+    
+    ///// ----- CONTRACT FUNCTIONS ----- /////
+    
     //Hirer deposits funds and initilizes the Gig contract
     function initGigContract(string memory gigID, Gig memory gigDetails) public payable returns (Status)
     {
@@ -51,7 +60,7 @@ contract GigsContract {
 
         //Gig is created and awaiting delivery
         gig[gigID].status = Status.AWAITING_DELIVERY;
-        totalValueLocked += gig[gigID].amount;
+        totalValueLocked += gig[gigID].amountDeposited;
         emit CreateGig(gigID, gig[gigID], "Gig created successfully.");
         return gig[gigID].status;
     }
@@ -68,8 +77,8 @@ contract GigsContract {
         require(myGig.status == Status.AWAITING_DELIVERY, "Gig status error.");
         
         //complete the gig anf payout
-        totalValueLocked -= myGig.amount;
-        freelancer.transfer(myGig.amount);
+        totalValueLocked -= myGig.amountDeposited;
+        freelancer.transfer(myGig.amountDeposited);
         myGig.status = Status.RESOLVED;
 
         emit CompleteGig(gigID, myGig, "Gig completed successfully.");
@@ -84,8 +93,8 @@ contract GigsContract {
         address payable hirer = myGig.hirer;
         
         require(myGig.status == Status.AWAITING_DELIVERY, "Gig status error.");
-        totalValueLocked -= myGig.amount;
-        hirer.transfer(myGig.amount);
+        totalValueLocked -= myGig.amountDeposited;
+        hirer.transfer(myGig.amountDeposited);
         myGig.status = Status.RESOLVED;
 
         emit ResolveGig(gigID, myGig, "Resolved to Hirer.");
@@ -100,11 +109,49 @@ contract GigsContract {
         address payable freelancer = myGig.freelancer;
         
         require(myGig.status == Status.AWAITING_DELIVERY, "Gig status error.");
-        totalValueLocked -= myGig.amount;
-        freelancer.transfer(myGig.amount);
+        totalValueLocked -= myGig.amountDeposited;
+        freelancer.transfer(myGig.amountDeposited);
         myGig.status = Status.COMPLETED;
 
         emit ResolveGig(gigID, myGig, "Resolved to Freelancer.");
+        return true;
+    }
+    
+    
+    
+    ///// ----- GETTER/SETTER FUNCTIONS ----- /////
+
+    //Get the details of a particular Gig contract 
+    function getGigDetails(address hirer, string memory gigID) public view returns (Gig memory)
+    {
+        return myGigs[hirer][gigID];
+    }
+    
+    //Update the USDC/stablecoin token contract address 
+    function setUSDCaddress(address a) public onlyAdmin returns (bool)
+    {
+        USDCaddress = a;
+        return true;
+    }
+    
+    //Update the WHIZ token contract address 
+    function setWHIZaddress(address a) public onlyAdmin returns (bool)
+    {
+        WHIZaddress = a;
+        return true;
+    }
+
+    //Update the ReviewsContract address 
+    function setREVIEWSaddress(address a) public onlyAdmin returns (bool)
+    {
+        REVIEWSaddress = a;
+        return true;
+    }
+    
+    //Update the amount of WHIZ rewards to be issued 
+    function setWHIZrewards(uint amt) public onlyAdmin returns (bool)
+    {
+        whizRewards = amt;
         return true;
     }
    
