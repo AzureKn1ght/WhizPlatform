33470e4945f8ce60dc95ddb622c5c2fb1f1839c4
Anwar <anwartan8@gmail.com>
2021-11-12 23:58:07 +0800

    updated href

--- frontend/js/middleware/metamask.js
+++ frontend/js/middleware/metamask.js
@@ -119,5 +119,76 @@ const changeHref = () => {
     dashboardNavBar.href = "dashboard-hirer.html";
   }
 };
+
+//create a function to check the accountId from session storage and change the href accordingly
+const checkAccountId = () => {
+  const accountId = sessionStorage.getItem("accountId");
+  //create const to select dom elements by id for elements "profileLink1" to "profileLink28"
+  const profileLink1 = document.getElementById("profileLink1");
+  const profileLink2 = document.getElementById("profileLink2");
+  const profileLink3 = document.getElementById("profileLink3");
+  const profileLink4 = document.getElementById("profileLink4");
+  const profileLink5 = document.getElementById("profileLink5");
+  const profileLink6 = document.getElementById("profileLink6");
+  const profileLink7 = document.getElementById("profileLink7");
+  const profileLink8 = document.getElementById("profileLink8");
+  const profileLink9 = document.getElementById("profileLink9");
+  const profileLink10 = document.getElementById("profileLink10");
+  const profileLink11 = document.getElementById("profileLink11");
+  const profileLink12 = document.getElementById("profileLink12");
+  const profileLink13 = document.getElementById("profileLink13");
+  const profileLink14 = document.getElementById("profileLink14");
+  const profileLink15 = document.getElementById("profileLink15");
+  const profileLink16 = document.getElementById("profileLink16");
+  const profileLink17 = document.getElementById("profileLink17");
+  const profileLink18 = document.getElementById("profileLink18");
+  const profileLink19 = document.getElementById("profileLink19");
+  const profileLink20 = document.getElementById("profileLink20");
+  const profileLink21 = document.getElementById("profileLink21");
+  const profileLink22 = document.getElementById("profileLink22");
+  const profileLink23 = document.getElementById("profileLink23");
+  const profileLink24 = document.getElementById("profileLink24");
+  const profileLink25 = document.getElementById("profileLink25");
+  const profileLink26 = document.getElementById("profileLink26");
+  const profileLink27 = document.getElementById("profileLink27");
+  const profileLink28 = document.getElementById("profileLink28");
+
+
+  if (accountId != "0xb88d7171a62a0c62e22f62af811c9c859fce4618") {
+    //set the href of all profile links to "#"
+    profileLink1.href = "#";
+    profileLink2.href = "#";
+    profileLink3.href = "#";
+    profileLink4.href = "#";
+    profileLink5.href = "#";
+    profileLink6.href = "#";
+    profileLink7.href = "#";
+    profileLink8.href = "#";
+    profileLink9.href = "#";
+    profileLink10.href = "#";
+    profileLink11.href = "#";
+    profileLink12.href = "#";
+    profileLink13.href = "#";
+    profileLink14.href = "#";
+    profileLink15.href = "#";
+    profileLink16.href = "#";
+    profileLink17.href = "#";
+    profileLink18.href = "#";
+    profileLink19.href = "#";
+    profileLink20.href = "#";
+    profileLink21.href = "#";
+    profileLink22.href = "#";
+    profileLink23.href = "#";
+    profileLink24.href = "#";
+    profileLink25.href = "#";
+    profileLink26.href = "#";
+    profileLink27.href = "#";
+    profileLink28.href = "#";
+  };
+};
+
+
+
 getUserName();
 changeHref();
+checkAccountId();
