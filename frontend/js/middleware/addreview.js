19d5910f725f4b566f916373bc1d06b724d0fc6f
Anwar <anwartan8@gmail.com>
2021-12-08 21:19:57 +0800

    review and clean up code

--- frontend/js/middleware/addreview.js
+++ frontend/js/middleware/addreview.js
@@ -1,4 +1,5 @@
 // (c) AzureKn1ght
+// define variables
 const title = document.getElementById("title");
 const description = document.getElementById("description");
 const deadline = document.getElementById("deadline");
@@ -23,14 +24,19 @@ const contract = new web3.eth.Contract(abi, conAddress);
 const meta = window.sessionStorage.getItem("accountId");
 var revieweeMeta = "";
 
+//function to fetch the details of the hirer
 const hirerDetails = async () => {
+  
+  //declare the url to call the api 
   let url =
     "https://ap-southeast-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/whiz-ihwsd/service/hirers/incoming_webhook/viewHirer";
 
+  // store the id of the hirer from the reviewee constant in the json object to send to the API 
   const hrData = {
     _id: reviewee,
   };
 
+  //try to fetch the data from the API
   try {
     const res = await fetch(url, {
       method: "POST",
@@ -40,25 +46,36 @@ const hirerDetails = async () => {
         "Content-Type": "application/json",
       },
     });
+
+     //if there is successful response, store the response in a variable
     const data = await res.json();
+
+    //assign the hirers metamask from the received json object to a variable
     revieweeMeta = data.metamask;
     console.log("reviewee meta is:" + data);
-  } catch (error) {
+
+
+  } catch (error) {             //catch any errors and log them to the console
     console.log(error);
   }
 };
 
-const gigDetails = async (e) => {
-  //e.preventDefault();
 
+// function to fetch the details of the job
+const gigDetails = async () => {
+  
+// declare the url to call the api
   let url =
     "https://ap-southeast-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/whiz-ihwsd/service/jobs/incoming_webhook/jobInfo";
 
+  // store the id of the job from the jobId constant in the json object to send to the API
   let idJob = jobId;
   const gigId = {
     _id: idJob,
   };
 
+
+ //try to fetch the data from the API
   try {
     const res = await fetch(url, {
       method: "POST",
@@ -68,8 +85,12 @@ const gigDetails = async (e) => {
         "Content-Type": "application/json",
       },
     });
+
+    //if there is successful response, store the response in a variable
     const jobs = await res.json();
     console.log(jobs);
+
+    //assign the job details from the received json object to a jobDetails object
     jobDetails = {
       jobID: jobs._id,
       budget: jobs.budget,
@@ -77,8 +98,9 @@ const gigDetails = async (e) => {
       description: jobs.description,
       skills: jobs.skills_required,
       location: jobs.required_location,
-    };
-
+    }
+    
+    //assign the deadline from the received json object to a variable and format it to the correct date format
     var date = moment(jobs.deadline).format("DD MMM YYYY");
     console.log(jobDetails);
 
@@ -90,66 +112,67 @@ const gigDetails = async (e) => {
     jobLocation.innerHTML = `${jobs.required_location}`;
     hirer.innerHTML = `${jobs.hirer_name}`;
 
-    if(jobs.gig_status == "PENDING"){
+    //hides the review button if the job status is open
+    if(jobs.gig_status == "OPEN"){
       submitReviewFreelancer.style.display = "none";
     }     
 
-    //Add to element
-  } catch (error) {
+    
+  } catch (error) {   //catch any errors and log them to the console
     console.log(error.message);
   }
 };
 
-//Goals (goal_id, description, balance, date_created, user_id);
 
-//Add Goal Function
-
-//Create Contract instance
+//function to add the review to the database
 const reviewAdd = async (e) => {
   e.preventDefault(); //to prevent form from submitting and refreshing the page
 
-  //to prevent empty input from submitting
+  
 
   //Step 1: Get the input data from the form
-  var t = parseInt(timeliness.options[timeliness.selectedIndex].value);
-  var q = parseInt(quality.options[quality.selectedIndex].value);
-  var o = parseInt(
+  var timelinessRating = parseInt(timeliness.options[timeliness.selectedIndex].value);
+  var qualityRating = parseInt(quality.options[quality.selectedIndex].value);
+  var overallRating = parseInt(
     overallHirerRating.options[overallHirerRating.selectedIndex].value
   );
 
-  var cR = parseInt(
+  var commRating = parseInt(
     communicationRating.options[communicationRating.selectedIndex].value
   );
-  var c = comments.value;
+  var comment = comments.value;
 
   var recommendation = parseInt(
     document.querySelector('input[name="recommendation"]:checked').value
   );
 
   var ratings = {
-    overall: o,
-    communication: cR,
-    quality: q,
-    timeliness: t,
+    overall: overallRating,
+    communication: commRating,
+    quality: qualityRating,
+    timeliness: timelinessRating,
     grading: recommendation,
   };
 
   console.log(ratings);
 
+  //store the input data in a json object to send to the API
   var input = {
     reviewID: jobId,
     reviewer: meta,
     reviewee: revieweeMeta,
     ratings: ratings,
-    comments: c,
+    comments: comment,
     job: jobDetails,
   };
 
   console.log(input);
 
+  //declare the url to call the api
   let url =
     "https://ap-southeast-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/whiz-ihwsd/service/flReview/incoming_webhook/flReview";
 
+  //try to send the data from the API
   try {
     const res = await fetch(url, {
       method: "POST",
@@ -161,62 +184,33 @@ const reviewAdd = async (e) => {
     });
 
   
+    //if there is successful response, store the response in a variable
     if(res.ok){
       const data = await res.json();
     console.log(data);
+
+    //alert the user that the review has been added
     alert(
       `Review submitted successfully!`
       );
+    //redirect the user to the their dashboard
       window.location.href= "dashboard-freelancer.html";
     
-    }else {
+    }else { //if there is an error, alert the user
     console.log(res);
     const message = await res.json();
     alert(message.error);
     };
 
-  } catch (error) {
+  } catch (error) { //catch any errors and log them to the console
     console.log(error);
   }
 
-  //Step 2: Send transaction to smart contract
-
-  /*  contract.methods
-    .addReview(input)
-    .send({
-      from: currentAccount,
-    })
-    .then((receipt) => {
-      console.log(receipt);
-      //window.location.href = "dashboard.html";
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-
-  //Alert user to wait for transactions
-  alert("Processing: sending your request");
-  console.log("Mark as complete: processing..."); */
 };
 
-const getReview = async (e) => {
-  let result = await contract.methods
-    .getAllReceivedReviews(currentAccount)
-    .call();
-  console.log(result);
-};
-
-const goBack = (e) => {
-  e.preventDefault();
-  alert("You are being redirected to the previous page");
-  window.history.go(-1);
-};
-
-//getReview(currentAccount);
 //Add event listener for buttons
 submitReviewFreelancer.addEventListener("click", reviewAdd);
-/* document.getElementById("b2").onclick = function () {
-  location.href = "dashboard.html";
-}; */
+
+//call the functions on page load
 hirerDetails();
 gigDetails();
